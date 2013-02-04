c36fb5be42e9c9d54113c3f1249e3ecf944cbf27
brunomars <nicholaschen35@gmail.com>
2013-02-04 02:26:54 -0500

    Full source code merge with ARM & ZGK

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -2,18 +2,12 @@
  * for MPU and CORE voltages
  */
 
-<<<<<<< HEAD
-#define OMAP3630_VDD_MPU_OPP50_UV               1000000
-#define OMAP3630_VDD_MPU_OPP200_UV              900000
-#define OMAP3630_VDD_MPU_OPP100_UV              1150000
+
+#define OMAP3630_VDD_MPU_OPP50_UV               980000
+#define OMAP3630_VDD_MPU_OPP100_UV              1100000
 #define OMAP3630_VDD_MPU_OPP120_UV              1275000
-#define OMAP3630_VDD_MPU_OPP900_UV              1350000
-=======
-#define OMAP3630_VDD_MPU_OPP50_UV               1012500
-#define OMAP3630_VDD_MPU_OPP100_UV              1200000
-#define OMAP3630_VDD_MPU_OPP120_UV              1325000
->>>>>>> bde3d31... cleaned all cpufreq and voltages
-#define OMAP3630_VDD_MPU_OPP1G_UV               1375000
+#define OMAP3630_VDD_MPU_OPP1G_UV               1300000
+#define OMAP3630_VDD_MPU_OPP1_2G_UV             1375000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
 #define OMAP3630_VDD_CORE_OPP100_UV             1200000
